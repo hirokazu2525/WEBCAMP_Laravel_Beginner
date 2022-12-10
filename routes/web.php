1b8753ba056a2d534bae1f4d114d7cba173f871b
kazumahirosawa <kazumahirosawa@example.com>
2022-12-10 09:38:43 +0000

    サンプルページのURLを変更した

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,11 @@ use App\Http\Controllers\WelcomeController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 /*
 Route::get('/', function () {
     return view('welcome');
 });
 */
-Route::get('/', [WelcomeController::class, 'index']);
+Route::get('/welcome', [WelcomeController::class, 'index']);
 Route::get('/welcome/second', [WelcomeController::class, 'second']);
